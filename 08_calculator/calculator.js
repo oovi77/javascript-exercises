d6a0f48438a4e556208b8014457f09259224361e
oovi77 <oovi77@gmail.com>
2022-06-15 12:03:50 -0400

    complete calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,69 @@
-const add = function() {
+const add = function(...args) {
+  let sum = 0;
+
+  for (let x in args) {
+    //console.log(' x is : ' + x);
+    sum += args[x];
+  }
+  return sum;
 	
 };
 
-const subtract = function() {
+const subtract = function(...args) {
+  return args[0] - args[1];
 	
 };
 
-const sum = function() {
+const sum = function(args) {
+
+  let sum = 0;
+
+  
+  for (let x in args) {
+    //console.log(` x is : ${x}`);
+    //console.log(` args is : ${args[x]}`);
+    sum += args[x];
+  }
+  
+  return sum;
 	
 };
 
-const multiply = function() {
+const multiply = function(args) {
+  let multiply = 1;
+
+  
+  for (let x in args) {
+    //console.log(` x is : ${x}`);
+    //console.log(` args is : ${args[x]}`);
+    multiply *= args[x];
+  }
+  
+  return multiply;
+
 
 };
 
-const power = function() {
+const power = function(a,b) {
+  return (a ** b);
 	
 };
 
-const factorial = function() {
+const factorial = function(num) {
+  let total = 1;
+
+  for (let i = 1; i <= num; i++) {
+    total = total * i;
+  }
+
+  return total;
 	
 };
+
+//console.log("test 12re");
+const temp = factorial(5);
+console.log(temp);
+console.log(typeof(temp));
 
 // Do not edit below this line
 module.exports = {
