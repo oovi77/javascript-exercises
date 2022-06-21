5979a486998024683270112eeeab1b013d4f15d2
oovi77 <oovi77@gmail.com>
2022-06-21 14:06:26 -0400

    add function to find the oldest person given a list of people objects

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,32 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = 0;
+    let oldestPerson = '';
+    curDate = (new Date()).getFullYear();
+    //console.log(`current date is: ${(curDate)}`);
+    
+    
+    for(let i = 0; i < people.length; i++){
+      
+      let age = 0;
+      
+      if(people[i].yearOfDeath === undefined) {
+        age = curDate - people[i].yearOfBirth;
+        
+        
+      } else {
+        age = people[i].yearOfDeath - people[i].yearOfBirth;
+      }
+      //console.log(`age is: ${age} , curDate is: ${curDate}`);
+      
+      if (age >= oldest) {
+        oldest = age;
+        oldestPerson = people[i];
+        //console.log(`oldestPerson is: ${oldestPerson}`);
+      }
+      
+      
+    }
+    return oldestPerson;
 
 };
 
